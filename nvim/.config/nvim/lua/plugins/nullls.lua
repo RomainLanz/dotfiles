0dd65468a5ab9a41a7c8f72d561f2909e949a787
Romain Lanz <romain.lanz@pm.me>
2023-08-14 19:46:54 +0200

    feat(nvim): add format on save

--- nvim/.config/nvim/lua/plugins/nullls.lua
+++ nvim/.config/nvim/lua/plugins/nullls.lua
@@ -4,12 +4,16 @@ return {
   config = function()
     local null_ls = require("null-ls")
 
+    local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
+    local event = "BufWritePre" -- or "BufWritePost"
+    local async = event == "BufWritePost"
+
     local check_eslint_config = function(utils)
-      return utils.root_has_file({ ".eslintrc", ".eslintrc.js" })
+      return utils.root_has_file({ ".eslintrc", ".eslintrc.js", ".eslintrc.cjs" })
     end
 
     local check_prettier_config = function(utils)
-      return utils.root_has_file({ ".prettierrc" })
+      return utils.root_has_file({ ".prettierrc", ".prettierrc.js", ".prettierrc.cjs", ".prettier.config.js" })
     end
 
     local sources = {
@@ -21,6 +25,33 @@ return {
       }),
       null_ls.builtins.formatting.stylua, }
 
-    null_ls.setup({ sources = sources, debug = true })
+    null_ls.setup({
+      sources = sources,
+      debug = true,
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.keymap.set("n", "<Leader>f", function()
+            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+          end, { buffer = bufnr, desc = "[lsp] format" })
+
+          -- format on save
+          vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+          vim.api.nvim_create_autocmd(event, {
+            buffer = bufnr,
+            group = group,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr, async = async })
+            end,
+            desc = "[lsp] format on save",
+          })
+        end
+
+        if client.supports_method("textDocument/rangeFormatting") then
+          vim.keymap.set("x", "<Leader>f", function()
+            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+          end, { buffer = bufnr, desc = "[lsp] format" })
+        end
+      end,
+    })
   end,
 }
