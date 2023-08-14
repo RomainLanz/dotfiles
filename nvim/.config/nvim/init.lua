983214d08833660a3ad9928378380fa21d6edcd2
Romain Lanz <romain.lanz@pm.me>
2023-08-14 19:27:16 +0200

    refactor: migrate to stow

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,28 @@
+-- VIM Options
+vim.opt.number = true
+vim.opt.showcmd = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+vim.g.mapleader = " "
+
+-- Package Manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- Load Plugins
+require("lazy").setup("plugins")
--- symlink.sh
+++ symlink.sh
@@ -13,26 +13,10 @@ if [[ $choice =~ ^[nN]$ ]]; then
   exit $?
 fi
 
-if [[ ! $(pwd) == "$HOME/.dotfiles" ]]; then
-  info 'Symlinking dotfile folder'
-  ln -sf $(pwd) "$HOME/.dotfiles"
-  success "linked $(pwd) to $HOME/.dotfiles"
-fi
-
-symlinker() {
-  info 'Symlinking dotfiles'
-
-  local overwrite=true overwrite_all=true backup_all=false skip_all=false
-
-  for file in $(find -H "$(pwd)" -maxdepth 5 -name '*.sym'); do
-    local path=$(grealpath --relative-to=$PWD $file)
-    mkdir -p "$HOME/$(dirname $path)"
-    local dest="$HOME/$(dirname $path)/$(basename "${file%.*}")"
-
-    ln -sf "$file" "$dest"	
-    success "linked $file to $dest"
-  done
-}
-
-symlinker
-
+stow gnupg
+stow asdf
+stow git
+stow nvim
+stow fish
+stow kitty
+stow omf
