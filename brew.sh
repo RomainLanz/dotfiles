7ee88ddfb7b60008cfc69e6272194933ce747fce
Romain Lanz <romain.lanz@pm.me>
2023-08-14 16:22:32 +0200

    feat: add golang

--- brew.sh
+++ brew.sh
@@ -112,6 +112,7 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
 mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
 asdf plugin-add python
 info 'Done'
 
