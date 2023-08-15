785c3ff3acf454421b04f0b5942ae4891a114ca5
Romain Lanz <romain.lanz@pm.me>
2023-08-15 10:57:02 +0200

    feat: use git_files to remove ignored file in telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
   keys = {
     { "<leader>fd", ":Telescope file_browser<CR>", silent = true, desc = "Open file browser (project dir)"},
     { "<leader>fb", ":Telescope file_browser path=%:p:h<CR>", silent = true, desc = "Open file browser (current dir)"},
-    { "<leader>ff", ":Telescope find_files<CR>", silent = true, desc = "Find file"},
+    { "<leader>ff", ":Telescope git_files<CR>", silent = true, desc = "Find file"},
     { "<leader>fg", ":Telescope live_grep<CR>", silent = true, desc = "Live grep"},
     { "<leader>bf", ":Telescope buffers<CR>", silent = true, desc = "Open buffer browser"},
   },
