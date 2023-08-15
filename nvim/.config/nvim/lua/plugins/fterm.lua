b1c1a2537639710a4f2c4abd4e38f08c653fcafc
Romain Lanz <romain.lanz@pm.me>
2023-08-15 10:56:42 +0200

    feat: change mapping to close figterm

--- nvim/.config/nvim/lua/plugins/fterm.lua
+++ nvim/.config/nvim/lua/plugins/fterm.lua
@@ -2,7 +2,6 @@ return {
   "numToStr/FTerm.nvim",
   keys = {
     { "<leader>t", '<CMD>lua require("FTerm").toggle()<CR>', desc = "Toggle terminal" },
-    { "<leader>t", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', mode = "t", desc = "Toggle terminal" },
-    { "<ESC><ESC>", "<C-\\><C-n>", mode = "t", desc = "Terminal normal mode" },
+    { "<ESC><ESC>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', mode = "t", desc = "Toggle terminal" },
   },
 }
