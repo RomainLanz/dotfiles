eb5c04408cfd084204bb15c3448b465d7b115f2b
Romain Lanz <romain.lanz@pm.me>
2023-08-16 12:07:39 +0200

    feat(nvim): remove auto formating

--- nvim/.config/nvim/lua/plugins/nullls.lua
+++ nvim/.config/nvim/lua/plugins/nullls.lua
@@ -28,30 +28,6 @@ return {
     null_ls.setup({
       sources = sources,
       debug = true,
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.keymap.set("n", "<Leader>f", function()
-            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-          end, { buffer = bufnr, desc = "[lsp] format" })
-
-          -- format on save
-          vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-          vim.api.nvim_create_autocmd(event, {
-            buffer = bufnr,
-            group = group,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr, async = async })
-            end,
-            desc = "[lsp] format on save",
-          })
-        end
-
-        if client.supports_method("textDocument/rangeFormatting") then
-          vim.keymap.set("x", "<Leader>f", function()
-            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-          end, { buffer = bufnr, desc = "[lsp] format" })
-        end
-      end,
     })
   end,
 }
