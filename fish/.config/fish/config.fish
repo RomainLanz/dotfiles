87f6da881983d6cd1079e468e59ae1f22f7719bb
Romain Lanz <romain.lanz@pm.me>
2024-04-07 16:12:21 +0200

    chore: update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,24 @@ set -gx GPG_TTY (tty)
 set -gx HOMEBREW_PREFIX "/opt/homebrew";
 set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
 set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+set -gx DOCKER_HOST "unix://$HOME/.colima/default/docker.sock"
+set -gx TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE "/var/run/docker.sock"
 
 set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
 set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
 set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
+# Go
+set -gx PATH "/Users/romainlanz/go/bin" $PATH;
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+alias k "kubectl"
+alias kcc "kubectl config current-context"
+alias kuc "kubectl config use-context"
